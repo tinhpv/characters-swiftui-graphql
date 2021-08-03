cce99872c566de361f6bddb820e94b2019c67c9d
tinhpv <vutinh.phan@gmail.com>
2021-08-03 23:21:54 +0700

    Initial Commit

---
+++ graphql_characters/graphql_charactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  graphql_charactersApp.swift
+//  graphql_characters
+//
+//  Created by TinhPV on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct graphql_charactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
